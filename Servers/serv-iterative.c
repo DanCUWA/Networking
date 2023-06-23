e9878439650cffdf254c5ca1e3bc0ea830548102
DanCUWA <chengdct@gmail.com>
2023-06-23 20:11:55 +0800

    init

--- Servers/serv-iterative.c
+++ Servers/serv-iterative.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8000
+int startServer(int PORT, int IPA){ 
+    int sfd;
+    if ((sfd = socket(AF_INET,SOCK_STREAM,0))<0) {
+        perror("Socket creation failed"); 
+        exit(EXIT_FAILURE);
+    };
+    
+}
 int main() { 
     printf("hello"); 
     return 0;
